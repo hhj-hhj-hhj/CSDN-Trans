985e4b923b4a6b26a6e847e35f8dc08c7516cdd2
HHJ <449216786@qq.com>
2024-09-24 10:53:27 +0800

    attention

--- core/train_attention.py
+++ core/train_attention.py
@@ -2,7 +2,7 @@ import torch
 from tools import MultiItemAverageMeter
 
 
-def train_stage0(base, dataloader):
+def train_stage0(base, dataloader, text_features):
     base.set_train()
     meter = MultiItemAverageMeter()
 
@@ -15,13 +15,13 @@ def train_stage0(base, dataloader):
 
         imgs = torch.cat([rgb_imgs, ir_imgs], dim=0)
 
-        target_t_p = torch.tensor([1]).to(base.device).long()
-        target_t_n = torch.tensor([2]).to(base.device).long()
-        target_t_p = target_t_p.repeat(rgb_imgs.size(0))
-        target_t_n = target_t_n.repeat(rgb_imgs.size(0))
-        target_t = torch.cat([target_t_p, target_t_n], dim=0)
+        # target_t_p = torch.tensor([1]).to(base.device).long()
+        # target_t_n = torch.tensor([2]).to(base.device).long()
+        # target_t_p = target_t_p.repeat(rgb_imgs.size(0))
+        # target_t_n = target_t_n.repeat(rgb_imgs.size(0))
+        # target_t = torch.cat([target_t_p, target_t_n], dim=0)
 
-        target_i = torch.tensor([1]).to(base.device).long()
+        target_i = torch.tensor([0]).to(base.device).long()
         target_i = target_i.repeat(2 * rgb_imgs.size(0))
         shape_imgs = torch.cat([shape_maps_rgb, shape_maps_ir], dim=0)
 
@@ -30,17 +30,19 @@ def train_stage0(base, dataloader):
             shape_maps = base.model(x1=shape_imgs, get_map=True)
 
         fusion_map = base.model(img_map=image_maps, shape_map=shape_maps, get_atten=True)
-        text_features_p = base.model.module.text_features_p
-        text_features_n = base.model.module.text_features_n
-        text_features_p = text_features_p.repeat(label1.size(0), 1)
-        text_features_n = text_features_n.repeat(label2.size(0) // 2, 1)
-        text_features = torch.cat([text_features_p, text_features_n], dim=0)
 
         base.model.module.attnpool.requires_grad = False
         image_features = base.model(fusion_map=fusion_map, maps2feature=True)
 
-        loss_i2t = base.con_creiteron(image_features, text_features, target_i, target_t)
-        loss_t2i = base.con_creiteron(text_features, image_features, target_t, target_i)
+        logit = image_features @ text_features.t()
+        loss_i2t = base.pid_creiteron(logit, target_i)
+        # loss_t2i = base.con_creiteron(text_features, image_features, target_t, target_i)
+        loss_t2i = loss_i2t
+
+        if torch.isnan(loss_i2t).any():
+            print("loss_i2t 中检测到 NaN")
+        if torch.isnan(loss_t2i).any():
+            print("loss_t2i 中检测到 NaN")
 
         loss = loss_i2t + loss_t2i
         base.model_optimizer_stage2.zero_grad()
@@ -49,7 +51,8 @@ def train_stage0(base, dataloader):
 
         meter.update({'loss_i2t': loss_i2t.data,
                       'loss_t2i': loss_t2i.data, })
-        print(f"[{i}/{len(dataloader)}] {meter.get_str()}")
+        if i % 10 == 0:
+            print(f"[{i}/{len(dataloader)}] {meter.get_str()}")
 
     return meter.get_val(), meter.get_str()
 
