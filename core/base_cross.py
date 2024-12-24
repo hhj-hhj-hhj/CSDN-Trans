43f31c33450f06f8c701f360c87cb093a0ed95ed
209 <449216786@qq.com>
2024-12-25 05:51:43 +0800

    ipd的margin改成 0.35

--- core/base_cross.py
+++ core/base_cross.py
@@ -72,7 +72,7 @@ class Base:
 
         self.criterion_hcc_kl_3 = hcc_kl_3(k2=1.2)
         self.IPC = IPC_v2(margin=0.6)
-        self.IPD = IPD(margin=0.4)
+        self.IPD = IPD(margin=0.35)
         # self.IPD_v2 = IPD_v2(t=0.1)
 
     def _init_optimizer_stage1(self):
