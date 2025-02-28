999215fc1e97bfc8b6969d14ae4770cea308e7f0
209 <449216786@qq.com>
2025-02-28 10:04:12 +0800

    ipd:1pc=1:1。best=74.5

--- core/base_cross.py
+++ core/base_cross.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from bisect import bisect_right
 from network.model_cross import Model
 from network.lr import CosineLRScheduler
-from tools import os_walk, CrossEntropyLabelSmooth, SupConLoss, TripletLoss_WRT, MSEL, MSEL_Feat, MSEL_Cos, IPD, IPC_v2,IPD_rgb, IPC_v2_rgb, IPD_rgb_ir, IPC_v2_rgb_ir
+from tools import os_walk, CrossEntropyLabelSmooth, SupConLoss, TripletLoss_WRT, IPD, IPC_v2
 
 def create_scheduler(optimizer, num_epochs, lr_min, warmup_lr_init, warmup_t, noise_range = None):
 
@@ -79,9 +79,9 @@ class Base:
         self.IPD = IPD(margin=0.3)
 
         # self.IPC_1 = IPC_v2_rgb_ir(margin=0.6)
-        self.IPC_1 = IPC_v2_rgb(margin=0.6)
+        # self.IPC_1 = IPC_v2_rgb(margin=0.6)
         # self.IPD_1 = IPD_rgb_ir(margin=0.3)
-        self.IPD_1 = IPD_rgb(margin=0.3)
+        # self.IPD_1 = IPD_rgb(margin=0.3)
 
     def _init_optimizer_stage1(self):
         params = []
